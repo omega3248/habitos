bece10c57b09490b4ff48f7b9b67d91254fc271b
omega3248 <152034539+omega3248@users.noreply.github.com>
2025-11-01 21:49:24 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('habi').then(cache => cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
